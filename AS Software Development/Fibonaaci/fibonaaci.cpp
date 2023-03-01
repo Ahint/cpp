4bf7b2bbf2edcee5f69e1ab406f89b207b38edaa
Ahint <91521097+Ahint@users.noreply.github.com>
2023-03-01 15:35:51 +0530

    Update fibonaaci.cpp

--- AS Software Development/Fibonaaci/fibonaaci.cpp
+++ AS Software Development/Fibonaaci/fibonaaci.cpp
@@ -6,30 +6,20 @@ void fibonaaci(int a)
 {
     int fibobox[a];
 
-    for (int i = 1; i <= a; i++)
+    fibobox[1] = 1;
+    fibobox[2] = 1;
+
+    for (int i = 3; i <= a; i++)
     {
-        if (i == 1 || i == 2)
-        {
-            fibobox[i] = 1;
-        }
-        else
-        {
-            fibobox[i] = (fibobox[i - 2] + fibobox[i - 1]);
-        }
+
+        fibobox[i] = (fibobox[i - 2] + fibobox[i - 1]);
     }
 
-    cout<<"\t\t";
-    
-   for (int b = 1; b <= a; b++)
+    cout << "\t\t";
+
+    for (int b = 1; b <= a; b++)
     {
-        if (b == a)
-        {
-            cout << fibobox[a]<<".";
-        }
-        else
-        {
-            cout << fibobox[b] << ",";
-        }
+        cout << fibobox[b] << " ";
     }
 }
 
@@ -38,14 +28,14 @@ int main()
     while (1)
     {
         int n;
-        cout << "\n\tEnter 0 to exit\n\tWARNING: Program is limited till 46th term.\n\tEnter the term till where you want fibonaaci series: ";
+        cout << "\n\tEnter 0 to exit\n\tWARNING: Program is limited till 46th term.\n\tEnter the term  where you want fibonaaci series: ";
         cin >> n;
 
-        if (n==0)
+        if (n == 0)
         {
             exit(0);
         }
-        
+
         fibonaaci(n);
     }
 
