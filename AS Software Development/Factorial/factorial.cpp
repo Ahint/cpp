dea509ab0898977b4d984af062aa43b52cc20ccc
Ahint <91521097+Ahint@users.noreply.github.com>
2023-03-11 00:07:05 +0530

    updated cpp

--- AS Software Development/Factorial/factorial.cpp
+++ AS Software Development/Factorial/factorial.cpp
@@ -2,16 +2,34 @@
 
 using namespace std;
 
+void display(int);
+
 int main()
 {
     int n;
-    cout << "Enter the number till where you want to print factorial series: ";
-    cin >> n;
 
+    while (1)
+    {
+        switch (n)
+        {
+        case 0:
+            exit(0);
+
+        default:
+            cout << "Enter 0 to exit \nEnter the number till where you want to print factorial series: ";
+            cin >> n;
+            display(n);
+        }
+    }
+}
+
+void display(int a)
+{
     int i = 1;
-    while (i <= n)
+    while (i <= a)
     {
         cout << i << " ";
         i++;
     }
+    cout << endl;
 }
